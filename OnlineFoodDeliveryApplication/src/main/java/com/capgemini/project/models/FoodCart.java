d272a5ed6942afe36637fb83f61ff5be475163fe
ganes <ganes@LAPTOP-85Q0AON1>
2022-04-20 19:31:02 +0530

    kvslganesh comitted

--- OnlineFoodDeliveryApplication/src/main/java/com/capgemini/project/models/FoodCart.java
+++ OnlineFoodDeliveryApplication/src/main/java/com/capgemini/project/models/FoodCart.java
@@ -1,5 +1,58 @@
 package com.capgemini.project.models;
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="food_cart")
 public class FoodCart {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	@OneToOne(cascade=CascadeType.ALL)
+	@JoinColumn(name="customerId")
+	private Customer cust;
+	@OneToMany(cascade=CascadeType.ALL,orphanRemoval=true,targetEntity=Item.class)
+	
+	private List<Item> item=new ArrayList<>();
+	public FoodCart() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public FoodCart(Long id, Customer cust, List<Item> item) {
+		super();
+		this.id = id;
+		this.cust = cust;
+		this.item = item;
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public Customer getCust() {
+		return cust;
+	}
+	public void setCust(Customer cust) {
+		this.cust = cust;
+	}
+	public List<Item> getItem() {
+		return item;
+	}
+	public void setItem(List<Item> item) {
+		this.item = item;
+	}
+	
+	
 
 }
