ade2cdce5aa60eb2374b500be55b87fe92eb5fb3
91926 <91926@LAPTOP-Q18PU8GH>
2022-04-21 15:47:52 +0530

    1547-2104

--- OnlineFoodDeliveryApplication/src/main/java/com/capgemini/project/models/Restaurant.java
+++ OnlineFoodDeliveryApplication/src/main/java/com/capgemini/project/models/Restaurant.java
@@ -1,5 +1,86 @@
 package com.capgemini.project.models;
 
+
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToOne;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
+@Entity
 public class Restaurant {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long restaurantId;
+    private String restaurantName;
+    @OneToOne(cascade=CascadeType.ALL)
+    @JoinColumn(name="addressId")
+    private Address address;
+    private String managerName;
+    private String contactNumber;
+    @ManyToMany(targetEntity=Item.class,mappedBy="res",cascade={CascadeType.PERSIST, CascadeType.DETACH,CascadeType.MERGE,CascadeType.REFRESH})
+    private List<Item> itemList;
+	public Restaurant() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public Restaurant(Long restaurantId, String restaurantName, Address address, String managerName,
+			String contactNumber,List<Item> itemList) {
+		super();
+		this.restaurantId = restaurantId;
+		this.restaurantName = restaurantName;
+		this.address = address;
+		this.managerName = managerName;
+		this.contactNumber = contactNumber;
+		this.itemList=itemList;
+	}
+	@JsonManagedReference
+	public List<Item> getItemList() {
+		return itemList;
+	}
+	public void setItemList(List<Item> itemList) {
+		this.itemList = itemList;
+	}
+	public Long getRestaurantId() {
+		return restaurantId;
+	}
+	public void setRestaurantId(Long restaurantId) {
+		this.restaurantId = restaurantId;
+	}
+	public String getRestaurantName() {
+		return restaurantName;
+	}
+	public void setRestaurantName(String restaurantName) {
+		this.restaurantName = restaurantName;
+	}
+	public Address getAddress() {
+		return address;
+	}
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+	public String getManagerName() {
+		return managerName;
+	}
+	public void setManagerName(String managerName) {
+		this.managerName = managerName;
+	}
+	public String getContactNumber() {
+		return contactNumber;
+	}
+	public void setContactNumber(String contactNumber) {
+		this.contactNumber = contactNumber;
+		
+	}
 	
+
 }
+
+
