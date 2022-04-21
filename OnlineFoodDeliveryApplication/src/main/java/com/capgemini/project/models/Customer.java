2198399be77f4f38db0d622f507f658c5d085ee1
DELL <DELL@DESKTOP-DBUC5JN>
2022-04-21 09:48:29 +0530

    from sravya

--- OnlineFoodDeliveryApplication/src/main/java/com/capgemini/project/models/Customer.java
+++ OnlineFoodDeliveryApplication/src/main/java/com/capgemini/project/models/Customer.java
@@ -1,5 +1,92 @@
 package com.capgemini.project.models;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+
+@Entity
 
 public class Customer {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private String customerId;
+	private String firstName;
+	private String lastName;
+	private int age;
+	private String gender;
+	@OneToOne(cascade=CascadeType.ALL)
+	@JoinColumn(name="addressId")
+	private Address address;
+	private String mobileNumber;
+	private String email;
+	public Customer() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public Customer(String customerId, String firstName, String lastName, int age,String gender, Address address, String mobileNumber,
+			String email) {
+		super();
+		this.customerId = customerId;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.age = age;
+		this.gender=gender;
+		this.address = address;
+		this.mobileNumber = mobileNumber;
+		this.email = email;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getCustomerId() {
+		return customerId;
+	}
+	public void setCustomerId(String customerId) {
+		this.customerId = customerId;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public Address getAddress() {
+		return address;
+	}
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+	public String getMobileNumber() {
+		return mobileNumber;
+	}
+	public void setMobileNumber(String mobileNumber) {
+		this.mobileNumber = mobileNumber;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
 	
+	
+
 }
