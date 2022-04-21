19dac80a78109c20883722a767b4db2ba2d9bfb3
91926 <91926@LAPTOP-Q18PU8GH>
2022-04-21 18:32:31 +0530

    vedant commited empty code

--- OnlineFoodDeliveryApplication/src/main/java/com/capgemini/project/models/Restaurant.java
+++ OnlineFoodDeliveryApplication/src/main/java/com/capgemini/project/models/Restaurant.java
@@ -18,69 +18,7 @@ import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 @Entity
 public class Restaurant {
-	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
-	private Long restaurantId;
-    private String restaurantName;
-    @OneToOne(cascade=CascadeType.ALL)
-    @JoinColumn(name="addressId")
-    private Address address;
-    private String managerName;
-    private String contactNumber;
-    @ManyToMany(targetEntity=Item.class,mappedBy="res",cascade={CascadeType.PERSIST, CascadeType.DETACH,CascadeType.MERGE,CascadeType.REFRESH})
-    private List<Item> itemList;
-	public Restaurant() {
-		
-	}
-	public Restaurant(Long restaurantId, String restaurantName, Address address, String managerName,
-			String contactNumber,List<Item> itemList) {
-		super();
-		this.restaurantId = restaurantId;
-		this.restaurantName = restaurantName;
-		this.address = address;
-		this.managerName = managerName;
-		this.contactNumber = contactNumber;
-		this.itemList=itemList;
-	}
-	@JsonManagedReference
-	public List<Item> getItemList() {
-		return itemList;
-	}
-	public void setItemList(List<Item> itemList) {
-		this.itemList = itemList;
-	}
-	public Long getRestaurantId() {
-		return restaurantId;
-	}
-	public void setRestaurantId(Long restaurantId) {
-		this.restaurantId = restaurantId;
-	}
-	public String getRestaurantName() {
-		return restaurantName;
-	}
-	public void setRestaurantName(String restaurantName) {
-		this.restaurantName = restaurantName;
-	}
-	public Address getAddress() {
-		return address;
-	}
-	public void setAddress(Address address) {
-		this.address = address;
-	}
-	public String getManagerName() {
-		return managerName;
-	}
-	public void setManagerName(String managerName) {
-		this.managerName = managerName;
-	}
-	public String getContactNumber() {
-		return contactNumber;
-	}
-	public void setContactNumber(String contactNumber) {
-		this.contactNumber = contactNumber;
-		
-	}
-	
+
 
 }
 
