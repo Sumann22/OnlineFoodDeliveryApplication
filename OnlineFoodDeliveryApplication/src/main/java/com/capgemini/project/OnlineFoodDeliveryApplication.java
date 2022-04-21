6646fb8331a3d6e8ebbdd6e0f16732cd3f8ca6c5
ganes <ganes@LAPTOP-85Q0AON1>
2022-04-21 11:11:31 +0530

    ok

--- OnlineFoodDeliveryApplication/src/main/java/com/capgemini/project/OnlineFoodDeliveryApplication.java
+++ OnlineFoodDeliveryApplication/src/main/java/com/capgemini/project/OnlineFoodDeliveryApplication.java
@@ -8,7 +8,7 @@ public class OnlineFoodDeliveryApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OnlineFoodDeliveryApplication.class, args);
-		System.out.println("connected ..");
+		System.out.println("connected ...");
 		
 	}
 
