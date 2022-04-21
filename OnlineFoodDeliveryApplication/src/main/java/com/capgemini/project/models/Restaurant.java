0ca8f8561f078e9f8b80c58a654e909b08c94ee4
91926 <91926@LAPTOP-Q18PU8GH>
2022-04-21 18:06:38 +0530

    Vedant committed

--- OnlineFoodDeliveryApplication/src/main/java/com/capgemini/project/models/Restaurant.java
+++ OnlineFoodDeliveryApplication/src/main/java/com/capgemini/project/models/Restaurant.java
@@ -3,6 +3,8 @@ package com.capgemini.project.models;
 
 import java.util.List;
 import java.util.*;
+
+import java.util.*;
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
